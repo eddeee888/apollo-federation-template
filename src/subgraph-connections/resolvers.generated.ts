f2e630f8dfe49c938d412622eeaa304d8b6b7aaf
Eddy Nguyen <github@eddeee888.me>
2024-11-28 23:42:34 +1100

    Remove ignored generated files

--- src/subgraph-connections/resolvers.generated.ts
+++
@@ -1,8 +0,0 @@
-/* This file was automatically generated. DO NOT UPDATE MANUALLY. */
-import type { Resolvers } from "./types.generated";
-import { Product } from "./resolvers/Product";
-import { User } from "./resolvers/User";
-export const resolvers: Resolvers = {
-  Product: Product,
-  User: User,
-};
--- src/subgraph-products/resolvers.generated.ts
+++
@@ -1,9 +0,0 @@
-/* This file was automatically generated. DO NOT UPDATE MANUALLY. */
-import type { Resolvers } from "./types.generated";
-import { product as Query_product } from "./resolvers/Query/product";
-import { Product } from "./resolvers/Product";
-export const resolvers: Resolvers = {
-  Query: { product: Query_product },
-
-  Product: Product,
-};
