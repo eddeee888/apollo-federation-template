a365cbacfdaffda37f3e06eae78c59e52c2ff7aa
Eddy Nguyen <github@eddeee888.me>
2025-01-27 15:58:08 +1100

    Add support for same subgraph interface

---
+++ src/subgraph-products/resolvers/Query/media.ts
@@ -0,0 +1,26 @@
+import type { QueryResolvers } from "./../../types.generated";
+export const media: NonNullable<QueryResolvers["media"]> = async (
+  _parent,
+  { id },
+  { database }
+) => {
+  console.log("*** products: Query.media ", id);
+  const mediaItem = database.media[id];
+  if (!mediaItem) {
+    return null;
+  }
+
+  if (mediaItem.type === "Book") {
+    return {
+      __typename: "Book",
+      id: mediaItem.id,
+      title: mediaItem.title,
+    };
+  }
+
+  return {
+    __typename: "Movie",
+    id: mediaItem.id,
+    name: mediaItem.title,
+  };
+};
